937f4545da9c06b2d1922bcf49f72ec0c7a2221a
Ajeesh77 <144651967+Ajeesh77@users.noreply.github.com>
2023-10-31 13:07:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:1500,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
